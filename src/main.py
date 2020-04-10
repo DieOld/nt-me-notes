7d20a09802fe5a4a3d0f661f1eb63c926f8186d2
vadym.tishchenko <vadym.tishchenko@raccoongang.com>
2020-04-10 10:35:19 +0300

    Add cors

--- src/main.py
+++ src/main.py
@@ -1,11 +1,22 @@
 from routes import routes
 from aiohttp.web import run_app, Application
 import middlewares
+import aiohttp_cors
 
 
 def create_app():
     app = Application(middlewares=middlewares.middleware_list)
     app.add_routes(routes)
+    cors = aiohttp_cors.setup(app, defaults={
+        "*": aiohttp_cors.ResourceOptions(
+            allow_credentials=True,
+            expose_headers="*",
+            allow_headers="*",
+        )
+    })
+
+    for route in list(app.router.routes()):
+        cors.add(route)
     return app
 
 
