6a83a625186be092febee8f81658e9dbb5da438a
vadym.tishchenko <vadym.tishchenko@raccoongang.com>
2020-04-10 22:20:50 +0300

    Update routes

--- src/routes.py
+++ src/routes.py
@@ -2,11 +2,11 @@ from aiohttp import web
 import api
 
 routes = [
-    web.get("/ping", api.ping),
-    web.post('/register', api.register),
-    web.post('/login', api.login),
-    web.post("/insert-note", api.insert_note),
-    web.patch("/update-note-state", api.update_note_state),
-    web.delete("/delete-note", api.delete_note),
-    web.get("/get-user-notes", api.get_user_notes)
+    web.get("api/ping", api.ping),
+    web.post('api/register', api.register),
+    web.post('api/login', api.login),
+    web.post("api/insert-note", api.insert_note),
+    web.patch("api/update-note-state", api.update_note_state),
+    web.delete("api/delete-note", api.delete_note),
+    web.get("api/get-user-notes", api.get_user_notes)
 ]
